e684237e700c5e2ee246f6883b5290d2f093ebc1
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 12:36:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,40 @@ class Song
     self.class.all << self
   end
 
+  def create
+    
+  end
+
+  def new_by_name
+
+  end
+
+  def create_by_name
+
+  end
+
+  def find_by_name
+
+  end
+
+  def find_or_create_by_name
+
+  end
+
+  def alphabetical
+
+  end
+
+  def new_from_filename
+
+  end
+
+  def create_from_filename
+
+  end
+
+  def destroy_all
+
+  end
+
 end
